e3917d5bc9400261a31d1ca5d3b60a5f54f61032
PrastowoAdi <sinaadi75@gmail.com>
2024-02-03 12:16:40 +0700

    feat: admin, client, server page

---
+++ src/components/auth/role-gate.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import { useCurrentRole } from '@/hooks/use-current-role';
+import { UserRole } from '@prisma/client';
+import { FormError } from '../form-error';
+
+interface RoleGateProps {
+  children: React.ReactNode;
+  allowedRole: UserRole;
+}
+
+export const RoleGate = ({ allowedRole, children }: RoleGateProps) => {
+  const role = useCurrentRole();
+
+  if (role !== allowedRole) {
+    <FormError message="You do not have permission to view this content!" />;
+  }
+  return <>{children}</>;
+};
